9290b6fd3099b8975573d363146f88e6bd0c4cca
Elisis-5002 <ana.elisa.bailon@gmail.com>
2026-05-28 11:38:14 -0600

    Inica el diseño del form menu

---
+++ BD_Escuela/frmMenu.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace BD_Escuela
+{
+    partial class frmMenu
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            archivoToolStripMenuItem = new ToolStripMenuItem();
+            salirToolStripMenuItem = new ToolStripMenuItem();
+            ventanasToolStripMenuItem = new ToolStripMenuItem();
+            profesoresToolStripMenuItem = new ToolStripMenuItem();
+            alumnosToolStripMenuItem = new ToolStripMenuItem();
+            cursosToolStripMenuItem = new ToolStripMenuItem();
+            asistenciaToolStripMenuItem = new ToolStripMenuItem();
+            label1 = new Label();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(24, 24);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { archivoToolStripMenuItem, ventanasToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 41);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            archivoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { salirToolStripMenuItem });
+            archivoToolStripMenuItem.Font = new Font("Times New Roman", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            archivoToolStripMenuItem.Size = new Size(119, 37);
+            archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // salirToolStripMenuItem
+            // 
+            salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            salirToolStripMenuItem.Size = new Size(270, 42);
+            salirToolStripMenuItem.Text = "Salir";
+            salirToolStripMenuItem.Click += salirToolStripMenuItem_Click;
+            // 
+            // ventanasToolStripMenuItem
+            // 
+            ventanasToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { profesoresToolStripMenuItem, alumnosToolStripMenuItem, cursosToolStripMenuItem, asistenciaToolStripMenuItem });
+            ventanasToolStripMenuItem.Font = new Font("Times New Roman", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ventanasToolStripMenuItem.Name = "ventanasToolStripMenuItem";
+            ventanasToolStripMenuItem.Size = new Size(131, 37);
+            ventanasToolStripMenuItem.Text = "Ventanas";
+            // 
+            // profesoresToolStripMenuItem
+            // 
+            profesoresToolStripMenuItem.Name = "profesoresToolStripMenuItem";
+            profesoresToolStripMenuItem.Size = new Size(237, 42);
+            profesoresToolStripMenuItem.Text = "Profesores";
+            // 
+            // alumnosToolStripMenuItem
+            // 
+            alumnosToolStripMenuItem.Name = "alumnosToolStripMenuItem";
+            alumnosToolStripMenuItem.Size = new Size(237, 42);
+            alumnosToolStripMenuItem.Text = "Alumnos";
+            // 
+            // cursosToolStripMenuItem
+            // 
+            cursosToolStripMenuItem.Name = "cursosToolStripMenuItem";
+            cursosToolStripMenuItem.Size = new Size(237, 42);
+            cursosToolStripMenuItem.Text = "Cursos";
+            // 
+            // asistenciaToolStripMenuItem
+            // 
+            asistenciaToolStripMenuItem.Name = "asistenciaToolStripMenuItem";
+            asistenciaToolStripMenuItem.Size = new Size(237, 42);
+            asistenciaToolStripMenuItem.Text = "Asistencia";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(232, 200);
+            label1.Name = "label1";
+            label1.Size = new Size(340, 41);
+            label1.TabIndex = 1;
+            label1.Text = "Base de Datos Escolar";
+            // 
+            // frmMenu
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "frmMenu";
+            Text = "Menu";
+            Load += frmMenu_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem archivoToolStripMenuItem;
+        private ToolStripMenuItem salirToolStripMenuItem;
+        private ToolStripMenuItem ventanasToolStripMenuItem;
+        private ToolStripMenuItem profesoresToolStripMenuItem;
+        private ToolStripMenuItem alumnosToolStripMenuItem;
+        private ToolStripMenuItem cursosToolStripMenuItem;
+        private ToolStripMenuItem asistenciaToolStripMenuItem;
+        private Label label1;
+    }
+}
---
+++ BD_Escuela/frmMenu.cs
@@ -0,0 +1,20 @@
+namespace BD_Escuela
+{
+    public partial class frmMenu : Form
+    {
+        public frmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMenu_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
